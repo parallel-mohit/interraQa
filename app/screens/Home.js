dddc13689b97d3e6323882d44807cb71389444b0
parallel-mohit <mohitrao1132@gmail.com>
2023-09-25 17:05:18 +0530

    sixth

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,97 +1,98 @@
 import { View, Text, StyleSheet, Image, SafeAreaView, TouchableOpacity, FlatList, ScrollView } from 'react-native'
+import RoundIconBtn from '../components/RoundIconBtn'
 
 export default Home = (props) => {
     let users = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]
-    let handleSeeAll = ()=>{
+    let handleSeeAll = () => {
         props.navigation.navigate('Projects')
     }
     return (
-        // <SafeAreaView >
-        <ScrollView style={styles.container}>
-            
-            <View style={styles.mainContainer}>
-                <View style={styles.lstContainer}>
-                    <View style={{ paddingVertical: 20 }}>
-                        <Text style={{
-                            fontSize: 16,
-                            marginLeft: 10
-                        }}>My Projects</Text>
-                        <FlatList data={users}
-                            scrollEnabled={false}
-                            renderItem={({ item }) => <View style={styles.list}>
-                                <View>
-                                    <View style={{ marginHorizontal: 10, marginVertical: 20 }}>
-                                        <Text style={{
-                                            fontSize: 16,
-                                        }}>PROJECT NAME</Text>
-                                    </View>
-                                    <View style={{
-                                        flexDirection: 'row', justifyContent: 'space-between', marginLeft: 10, marginRight: 10, marginTop: 12
-                                    }}>
-                                        <Text style={{ color: '#212121', fontSize: 12 }}>CLIENT</Text>
-                                        <Text style={{ color: '#212121', fontSize: 12 }}>END DATE</Text>
-                                    </View>
-                                    <View style={{
-                                        flexDirection: 'row', justifyContent: 'space-between', marginLeft: 10, marginRight: 10, marginTop: 5
-                                    }}>
-                                        <Text style={{ color: '#212121', fontSize: 14 }}>CLIENT NAME</Text>
-                                        <Text style={{ color: '#212121', fontSize: 14 }}>Thu, 4 Aug 2022</Text>
-                                    </View>
-                                    <View style={{
-                                        flexDirection: 'row', justifyContent: 'space-between', marginLeft: 10, marginRight: 10, marginTop: 5
-                                    }}>
-                                        <Text style={{ color: '#212121', fontSize: 14 }}>Bugs</Text>
+        <>
+            <ScrollView style={styles.container}>
 
-                                    </View>
-                                    <View style={{
-                                        flexDirection: 'row', justifyContent: 'space-between', marginLeft: 10, marginRight: 10, marginTop: 5
-                                    }}>
-                                    </View>
-                                    <View style={{
-                                        flexDirection: 'row',
+                <View style={styles.mainContainer}>
+                    <View style={styles.lstContainer}>
+                        <View style={{ paddingVertical: 20 }}>
+                            <Text style={{
+                                fontSize: 16,
+                                marginLeft: 10
+                            }}>My Projects</Text>
+                            <FlatList data={users}
+                                scrollEnabled={false}
+                                renderItem={({ item }) => <View style={styles.list}>
+                                    <View>
+                                        <View style={{ marginHorizontal: 10, marginVertical: 20 }}>
+                                            <Text style={{
+                                                fontSize: 16,
+                                            }}>PROJECT NAME</Text>
+                                        </View>
+                                        <View style={{
+                                            flexDirection: 'row', justifyContent: 'space-between', marginLeft: 10, marginRight: 10, marginTop: 12
+                                        }}>
+                                            <Text style={{ color: '#212121', fontSize: 12 }}>CLIENT</Text>
+                                            <Text style={{ color: '#212121', fontSize: 12 }}>END DATE</Text>
+                                        </View>
+                                        <View style={{
+                                            flexDirection: 'row', justifyContent: 'space-between', marginLeft: 10, marginRight: 10, marginTop: 5
+                                        }}>
+                                            <Text style={{ color: '#212121', fontSize: 14 }}>CLIENT NAME</Text>
+                                            <Text style={{ color: '#212121', fontSize: 14 }}>Thu, 4 Aug 2022</Text>
+                                        </View>
+                                        <View style={{
+                                            flexDirection: 'row', justifyContent: 'space-between', marginLeft: 10, marginRight: 10, marginTop: 5
+                                        }}>
+                                            <Text style={{ color: '#212121', fontSize: 14 }}>Bugs</Text>
 
-                                    }}>
-                                        <View style={[styles.btn, { backgroundColor: '#FF5722' }]}>
-                                            <Text>11 High</Text>
                                         </View>
-                                        <View style={[styles.btn, { backgroundColor: '#FF5722' }]}>
-                                            <Text>16 Critical</Text>
+                                        <View style={{
+                                            flexDirection: 'row', justifyContent: 'space-between', marginLeft: 10, marginRight: 10, marginTop: 5
+                                        }}>
                                         </View>
-                                        <View style={[styles.btn, { backgroundColor: '#FFC107' }]}>
-                                            <Text>4 Major</Text>
+                                        <View style={{
+                                            flexDirection: 'row',
+
+                                        }}>
+                                            <View style={[styles.btn, { backgroundColor: '#FF5722' }]}>
+                                                <Text>11 High</Text>
+                                            </View>
+                                            <View style={[styles.btn, { backgroundColor: '#FF5722' }]}>
+                                                <Text>16 Critical</Text>
+                                            </View>
+                                            <View style={[styles.btn, { backgroundColor: '#FFC107' }]}>
+                                                <Text>4 Major</Text>
+                                            </View>
                                         </View>
                                     </View>
-                                </View>
-                            </View>} />
+                                </View>} />
+
+                        </View>
+
+                    </View>
+                    <View style={styles.diagramView}>
+                        <View style={{ marginHorizontal: 10, marginVertical: 20 }}>
+                            <Text style={{
+                                color: '#616161', fontSize: 14
+                            }}>BUG STATUS</Text>
+                        </View>
+
+                    </View>
+                    <View style={styles.diagramView}>
+                        <View style={{ marginHorizontal: 10, marginVertical: 20 }}>
+                            <Text style={{
+                                color: '#616161', fontSize: 14
+                            }}>PROJECT STATUS</Text>
+                            <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => handleSeeAll()}>
+                                <Text>SEE ALL</Text>
+                            </TouchableOpacity>
+                        </View>
 
                     </View>
 
                 </View>
-                <View style={styles.diagramView}>
-                    <View style={{marginHorizontal:10,marginVertical:20}}>
-                        <Text style={{
-                            color:'#616161',fontSize:14
-                        }}>BUG STATUS</Text>
-                    </View>
 
-                </View>
-                <View style={styles.diagramView}>
-                    <View style={{marginHorizontal:10,marginVertical:20}}>
-                        <Text style={{
-                            color:'#616161',fontSize:14
-                        }}>PROJECT STATUS</Text>
-                        <TouchableOpacity style={{alignItems:'center'}} onPress={()=>handleSeeAll()}>
-                            <Text>SEE ALL</Text>
-                        </TouchableOpacity>
-                    </View>
-
-                </View>
-
-            </View>
-
-        </ScrollView>
-        // </SafeAreaView>
-
+            </ScrollView>
+            <RoundIconbtn name={'plus'} color={'#FFFFFF'} size={16} style={styles.addbtn} />
+        </>
     )
 }
 
@@ -112,7 +113,7 @@ let styles = StyleSheet.create({
         marginHorizontal: 10
     },
     lstContainer: {
-       
+
         marginTop: 20,
         borderRadius: 5,
         backgroundColor: 'white',
@@ -143,11 +144,20 @@ let styles = StyleSheet.create({
     }, diagramView: {
         width: '100%',
         height: 568,
-        marginVertical:10,
+        marginVertical: 10,
         borderRadius: 5,
         backgroundColor: 'white',
         borderWidth: 1,
         borderColor: '#E0E0E0',
 
+    },
+    addbtn: {
+        position: 'absolute',
+        bottom: 40,
+        right: 25,
+        width: 56,
+        height: 56,
+        textAlign: 'center',
+        verticalAlign: 'middle'
     }
 })
--- app/screens/TestCaseId.js
+++ app/screens/TestCaseId.js
@@ -1,11 +1,106 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput, ScrollView, TouchableOpacity } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 
-export default TestCaseId  = () => {
-return (
-    <View style={styles.container}></View>
+
+export default TestCaseId = () => {
+    let handleImagePicker = async () => {
+        let result = await launchImageLibrary({ mediaType: 'photo', quality: 0 });
+        // console.log(result.assets)
+    }
+    return (
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.container} >
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-between',
+                }}>
+                    <Text style={{ fontSize: 12, color: '#616161' }}>TEST CASE ID</Text>
+                    <Text style={{ fontSize: 12, color: '#616161' }}>USE CASE ID</Text>
+                </View>
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-between', marginTop: 5
+                }}>
+                    <Text style={{ fontSize: 14, color: '#212121' }}>TC004</Text>
+                    <Text style={{ fontSize: 14, color: '#212121' }}>UC004</Text>
+                </View>
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-between', marginTop: 10
+                }}>
+                    <Text style={{ fontSize: 12, color: '#616161' }}>TYPE</Text>
+                    <Text style={{ fontSize: 12, color: '#616161' }}>TEST BEHAVIOUR</Text>
+                </View>
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-between', marginTop: 10
+                }}>
+                    <View style={[styles.btn, { backgroundColor: '#2196F3', width: 42 }]} >
+                        <Text style={{ color: '#FFFFFF', fontSize: 12 }}>UI</Text>
+                    </View>
+                    <View style={[styles.btn, { backgroundColor: '#4CAF50' }]} >
+                        <Text style={{ color: '#ffffff', fontSize: 12 }}>POSITIVE</Text>
+                    </View>
+                </View>
+                <View style={{ borderWidth: 0.4, borderColor: '#BDBDBD', marginBottom: 20 }}></View>
+
+                <Text style={{ fontSize: 12, color: "#616161" }}>PRE CONDITION</Text>
+                <Text style={{ fontSize: 14, color: '#212121', marginTop: 5 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                <Text style={{ fontSize: 12, color: '#616161', marginTop: 10 }}>TEST SUMMARY</Text>
+                <Text style={{ fontSize: 14, color: '#212121', marginTop: 5 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                <Text style={{ fontSize: 12, color: "#616161", marginTop: 10 }}>STEPS</Text>
+                <Text style={{ fontSize: 14, color: '#212121', marginTop: 5 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                <Text style={{ fontSize: 12, color: "#616161", marginTop: 10 }}>EXPECTED RESULTS</Text>
+                <Text style={{ fontSize: 14, color: '#212121', marginTop: 5 }}>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                <View style={{ borderWidth: 1, borderColor: '#BDBDBD', marginBottom: 20, marginTop: 20 }}></View>
+                <Text style={{ color: '#616161', fontSize: 12 }}>RESULTS</Text>
+                <View style={{
+                    flexDirection: 'row', marginTop: 10
+                }}>
+                    <View style={[styles.btn, { backgroundColor: '#4CAF50', height: 40, width: 80, borderRadius: 5 }]} >
+                        <Text style={{ color: 'white', fontSize: 12 }}>PASS</Text>
+                    </View>
+                    <View style={[styles.btn, { backgroundColor: '#FF5722', height: 40, width: 80, marginLeft: 20, borderRadius: 5 }]} >
+                        <Text style={{ color: 'white', fontSize: 12 }}>FAIL</Text>
+                    </View>
+                </View>
+                <Text style={{ fontSize: 12, color: "#616161", marginTop: 10 }}>ACTUAL RESULT</Text>
+                <TextInput style={styles.input} >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmo
+                    d tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</TextInput>
+                <View style={{ borderWidth: 0.4, borderColor: '#BDBDBD', marginBottom: 20, marginTop: 20 }}></View>
+                <Text style={{ fontSize: 14, color: '#616161', fontWeight: '600' }}>ATTACHMENTS</Text>
+                <TouchableOpacity onPress={handleImagePicker}>
+                    <View style={{
+                        flexDirection: 'row', alignItems: 'center',
+                        backgroundColor: '#03A9F4', height: 40, width: 170, borderRadius: 5, marginTop: 10
+                    }} >
+                        <AntDesign name='camera' size={20} color={'white'} style={{ marginLeft: 20 }} />
+                        <Text style={{ color: 'white', fontSize: 16, fontWeight: '700', marginLeft: 10 }}>ATTACH FILE</Text>
+                    </View>
+
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
     )
 }
 
 let styles = StyleSheet.create({
-    container:{},
+    container: {
+        paddingHorizontal: 20,
+        paddingVertical: 20
+    },
+    btn: {
+        height: 23,
+        width: 70,
+        borderRadius: 30,
+
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20
+    }, input: {
+
+        backgroundColor: '#FFFFFF',
+        borderWidth: 1,
+        borderRadius: 4,
+        paddingVertical: 20,
+        paddingHorizontal: 20
+    }
 })
